e4b3c0743efcc22bcfc6f545d80968a8e97b7240
Sohag-84 <ihsohag84@gmail.com>
2025-10-03 19:30:08 +0600

    check user login or not

--- lib/app/modules/splash/controllers/splash_controller.dart
+++ lib/app/modules/splash/controllers/splash_controller.dart
@@ -1,14 +1,20 @@
 import 'dart:async';
 
 import 'package:expense_tracker/app/routes/app_pages.dart';
+import 'package:expense_tracker/core/constant/firebase_constant.dart';
 import 'package:get/get.dart';
 
 class SplashController extends GetxController {
   @override
   void onInit() {
     super.onInit();
+
     Timer(Duration(seconds: 3), () {
-      Get.offNamed(Routes.LOGIN);
+      if (currentUser?.uid != null) {
+        Get.offNamed(Routes.HOME);
+      } else {
+        Get.offNamed(Routes.LOGIN);
+      }
     });
   }
 }
